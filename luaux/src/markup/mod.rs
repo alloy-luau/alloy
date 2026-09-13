ee8a427e64596c9bd44cbe620aa2647e6ce0704d
savruun <kunosyn@gmail.com>
2026-09-13 04:57:01 -0500

    style(markup): the re-export sorts as the crate's edition wants

--- luaux/src/markup/mod.rs
+++ luaux/src/markup/mod.rs
@@ -2,4 +2,4 @@ pub mod ast;
 pub mod parser;
 
 pub use ast::*;
-pub use parser::{ParseError, opens_a_statement_line, parse_node, statement_line_after};
+pub use parser::{opens_a_statement_line, parse_node, statement_line_after, ParseError};
